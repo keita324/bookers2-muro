7abedde6c1c561c20582c5974ad795a8d6af3ed8
EC2 Default User <ec2-user@ip-172-31-11-87.ap-northeast-1.compute.internal>
2023-01-22 07:23:44 +0000

    favorites if文追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.new(book_id: book.id)
-    @favorite.save
-    render 'replace_btn'
+    if !Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+      ## favoriteがなければ
+      book = Book.find(params[:book_id])
+      @favorite = current_user.favorites.new(book_id: book.id)
+      @favorite.save
+      render 'replace_btn'
+
+    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.find_by(book_id: book.id)
-    @favorite.destroy
-    render 'replace_btn'
+    if Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+      book = Book.find(params[:book_id])
+      @favorite = current_user.favorites.find_by(book_id: book.id)
+      @favorite.destroy
+      render 'replace_btn'
+    end
   end
 end
